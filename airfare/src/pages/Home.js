a2e8513dd8a2e927248d1eab38e2693b2a3f5b06
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2024-12-29 19:12:00 +1000

    updated css
    added a bit of decoration

--- airfare/src/pages/Home.js
+++ airfare/src/pages/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../elements/Navbar";
+import "../styles/Home.css"
 function Home() {
   return (
     <div className="Home">
       <Navbar/>
       <p>Welcome to DailyFares.</p>
+      <p>Hello there!</p>
     </div>
   );
 }
